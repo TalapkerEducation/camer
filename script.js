2769c8d11f78d1a1102f11fc8df98e1488f48d08
TalapkerEducation <aidosivan1@gmail.com>
2024-08-19 01:01:46 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let numButtonClicks = 0;
+
+function buttonClicked() {
+    numButtonClicks = numButtonClicks + 1;
+    document.getElementById("mainDiv").textContent =
+        "Button Clicked times: " + numButtonClicks;
+}
+
+let human = {
+    "name": "Darkhan",
+    "family name": "Nazarov",
+    "age": 27,
+    "job": "сантехник"
+};
